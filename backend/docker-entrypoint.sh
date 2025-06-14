f9a1e56211d560d0490affb44325469ee416a33a
Mike Chester <mike@chester.io>
2025-06-13 22:00:17 -0700

    maybe

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -9,9 +9,5 @@ python -m alembic upgrade head
 # Create initial data in DB
 python kubestats/initial_data.py
 
-# Calculate number of workers
-WORKERS=${WORKERS:-$(($(nproc) * 2 + 1))}
-LOG_LEVEL=${LOG_LEVEL:-info}
-
 # Start Gunicorn with inline configuration
-exec fastapi run kubestats/main.py --port 80
+exec fastapi run kubestats/main.py --port 8000
