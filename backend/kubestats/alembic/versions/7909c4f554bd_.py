d4c83f9082f580c699413138f38daca30e9d0be8
Mike Chester <mike@chester.io>
2025-06-13 21:23:14 -0700

    maybe good

---
+++ backend/kubestats/alembic/versions/7909c4f554bd_.py
@@ -0,0 +1,25 @@
+"""empty message
+
+Revision ID: 7909c4f554bd
+Revises: 265415657ac3
+Create Date: 2025-06-11 22:26:44.253219
+
+"""
+from alembic import op
+import sqlalchemy as sa
+import sqlmodel.sql.sqltypes
+
+
+# revision identifiers, used by Alembic.
+revision = '7909c4f554bd'
+down_revision = '265415657ac3'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
